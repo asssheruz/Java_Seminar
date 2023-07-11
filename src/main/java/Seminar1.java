dd26a5d91435315ada03d8aafe545c92c96a8f8b
Java_DZ <asssher.uz@gmail.com>
2023-07-11 20:53:50 +0500

    Added Seminar1

---
+++ src/main/java/Seminar1.java
@@ -0,0 +1,36 @@
+public class Seminar1 {
+    long along;
+    byte aByte;
+    short aShort;
+    char aChar;
+    float aFloat;
+    double aDouble;
+    boolean aBoolean;
+    int[] ints = new int[5];
+
+
+    public static void main(String[] args) {
+//        int[] ints = new int[5];
+//        ints[3] = 2;
+//
+//        int[] tmp = new int[ints.length + 1];
+//        ints = tmp;
+//
+//        if (true) {
+//            ints[0]++;} else if (1>5) {
+//            ints[0]--;} else {
+//            ints[0]=0;}
+//        int b = 0;
+//        int a = 12;
+//        b= --a - a--; //49-49
+//        b= a-- - --a;   //50-48
+//        System.out.println(b);
+
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
+
+
+
+    }
+}
